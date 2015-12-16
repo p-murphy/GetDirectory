845b4750e71ee1c4013d61b4ef8049dd163613fa
Patrick Murphy <patrick.watari@gmail.com>
2015-12-15 18:35:50 -0800

    Create Directory class
    The Directory class is the basic unit of data and hides implementation
    of Direct.h.

--- GetDirectory/GetDirectory.cpp
+++ GetDirectory/GetDirectory.cpp
@@ -8,77 +8,55 @@
 #include <string.h>
 
 
-int GetCurrentDirectory(char dir[]);
-int PrintCurrentDirectory(char dir[]);
-int SetCurrentDirectory(char dir[]);
+//// CLASSES ////
 
+class Directory
+{
+public:
+	char directory[FILENAME_MAX];
+	char *dir;
+	int size = sizeof(directory);
+};
+
+//// FUNCTIONS ////
+
+// Load current directory information into Directory object dir
+void GetCurrentDirectory(Directory *dir);
+
+// Print current directory path to cout
+void PrintCurrentDirectory();
+
+// Print path information in Directory object dir
+void PrintDirectory(Directory *dir);
+
+// Prompt user to take a directory action
+void MenuSelection();
+
+// Accept user choide for directory action
+int GetUserChoice();
+
+
+int SetCurrentDirectory(char dir[]);
 void GetParentDirectory(char parent[], char child[]);
 int PrintParentDirectory(char dirParent[], char dirChild[]);
-
-int GetUserChoice()
-{
-	int userChoice;
-	std::cin >> userChoice;
-	return userChoice;
-}
-
-void MenuSelection()
-{
-	int userChoice;
-
-	std::cout << "Please choose one of the following:" << std::endl;
-	std::cout << "1 - Print Current Directory Path" << std::endl;
-	std::cout << "2 - Print Current Directory Contents" << std::endl;
-	std::cout << "3 - Print Parent Directory Path" << std::endl;
-	std::cout << "4 - Print Parent Directory Contents" << std::endl;
-	std::cout << "5 - Change to Parent Directory" << std::endl;
-	std::cout << "6 - End Program" << std::endl;
-
-	userChoice = GetUserChoice();
-
-	switch(userChoice)
-	{
-	case 0 :
-		std::cout << "User has selected: " << userChoice << std::endl;
-		break;
-	case 1 : 
-		std::cout << "User has selected: " << userChoice << std::endl;
-		break;
-	case 2 : 
-		std::cout << "User has selected: " << userChoice << std::endl;
-		break;
-	case 3 : 
-		std::cout << "User has selected: " << userChoice << std::endl;
-		break;
-	case 4 : 
-		std::cout << "User has selected: " << userChoice << std::endl;
-		break;
-	case 5 : 
-		std::cout << "User has selected: " << userChoice << std::endl;
-		break;
-	case 6 : 
-		std::cout << "User has selected: " << userChoice << std::endl;
-		break;
-	}
-}
-
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	char workingDirectory[FILENAME_MAX];
-	char cCurrentPath[FILENAME_MAX];
+	char *workingDirectory3;
+	char directoryBuffer2[FILENAME_MAX];
 	char newPath[FILENAME_MAX];
 
-	//MenuSelection();
-	try {
-		GetCurrentDirectory(cCurrentPath);
-	} catch( const std::exception& e ) { // reference to the base of a polymorphic object
-     std::cout << e.what(); // information from length_error printed
-}
-	
+	Directory *workingDirectory = new Directory();
+	Directory *workingDirectory2 = new Directory();
 
-	GetParentDirectory(newPath, cCurrentPath);
+	GetCurrentDirectory(workingDirectory);
+	PrintDirectory(workingDirectory);
+	PrintCurrentDirectory();
+
+	std::cout << "/////////////////////" << std::endl;
+
+	/*GetParentDirectory(newPath, directoryBuffer);
 	std::cout << "GetParentDirectory: " << newPath << std::endl;
     _chdir(newPath);
     std::cout << "SYSTEM///////////////////////" << std::endl;
@@ -123,12 +101,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	std::cout << "NewPath length: " << len << std::endl;
 
 
-	if (!_getcwd(cCurrentPath, sizeof(cCurrentPath)))
-    {
-		return errno;
-    }
-
-	std::cout << "Current working directory: " << cCurrentPath << std::endl;
+	std::cout << "Current working directory: " << workingDirectory << std::endl;*/
 
 
 	std::cin.ignore(32767, '\n');
@@ -137,18 +110,30 @@ int _tmain(int argc, _TCHAR* argv[])
 	return 0;
 }
 
-int GetCurrentDirectory(char dir[])
+
+void GetCurrentDirectory(Directory *dir)
 {
-	if (!_getcwd(dir, sizeof(dir)))
-    {
-		std::cout << "Error in GetCurrentDirectory." << std::endl;
-		return errno;
-    }
-
-    dir[sizeof(dir) - 1] = '\0';
-	return 0;
-
+	dir->dir = _getcwd(dir->directory, dir->size);
 }
+
+void PrintCurrentDirectory()
+{
+	char directoryBuffer[FILENAME_MAX];
+	std::cout << _getcwd(directoryBuffer, sizeof(directoryBuffer)) << std::endl;
+}
+
+void PrintDirectory(Directory *dir)
+{
+	if (dir->dir == nullptr)
+	{
+		std::cout << "Error: Directory object is not initialized..." << std::endl;
+		return;
+	}
+
+	std::cout << _getcwd(dir->directory, dir->size) << std::endl;
+}
+
+
 
 void GetParentDirectory(char parent[], char child[])
 {
@@ -180,22 +165,57 @@ int SetCurrentDirectory(char dir[])
 	return 0;
 }
 
-int PrintCurrentDirectory(char dir[])
-{
-	if(!GetCurrentDirectory(dir))
-	{
-		return errno;
-	}
-
-	std::cout << dir << std::endl;
-
-	return 0;
-}
-
 int PrintParentDirectory(char dirParent[], char dirChild[])
 {
 	void GetParentDirectory(char dirParent[], char dirChild[]);
 	std::cout << dirParent << std::endl;
 
 	return 0;
+}
+
+void MenuSelection()
+{
+	int userChoice;
+
+	std::cout << "Please choose one of the following:" << std::endl;
+	std::cout << "1 - Print Current Directory Path" << std::endl;
+	std::cout << "2 - Print Current Directory Contents" << std::endl;
+	std::cout << "3 - Print Parent Directory Path" << std::endl;
+	std::cout << "4 - Print Parent Directory Contents" << std::endl;
+	std::cout << "5 - Change to Parent Directory" << std::endl;
+	std::cout << "6 - End Program" << std::endl;
+
+	userChoice = GetUserChoice();
+
+	switch (userChoice)
+	{
+	case 0:
+		std::cout << "User has selected: " << userChoice << std::endl;
+		break;
+	case 1:
+		std::cout << "User has selected: " << userChoice << std::endl;
+		break;
+	case 2:
+		std::cout << "User has selected: " << userChoice << std::endl;
+		break;
+	case 3:
+		std::cout << "User has selected: " << userChoice << std::endl;
+		break;
+	case 4:
+		std::cout << "User has selected: " << userChoice << std::endl;
+		break;
+	case 5:
+		std::cout << "User has selected: " << userChoice << std::endl;
+		break;
+	case 6:
+		std::cout << "User has selected: " << userChoice << std::endl;
+		break;
+	}
+}
+
+int GetUserChoice()
+{
+	int userChoice;
+	std::cin >> userChoice;
+	return userChoice;
 }
